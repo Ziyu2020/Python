d225dda0f17f1addf5ff1997385a00b1725edf03
Ziyu <64806343+Ziyu2020@users.noreply.github.com>
2020-05-20 22:58:45 +0200

    Update 20_05_2020.py
    Exercise about the def()

--- 20_05_2020.py
+++ 20_05_2020.py
@@ -1,1 +1,16 @@
-improt numpy as py
+def gcd(x,y):
+    """the minimal factor"""
+    (x, y) = (y, x) if x > y else (x, y)
+    for factor in range(y, 0, -1):
+        if x % factor == 0 and y % factor == 0:
+            return factor
+
+def lcm(x,y):
+    """the least common multiple"""
+    common_multiple = x * y // gcd(x,y)
+    return common_multiple
+
+
+x = int(input('x = '))
+y = int(input('y = '))
+print('the minimal of them is %d, the least common multiple is %d' % (gcd(x,y), lcm(x,y)))
